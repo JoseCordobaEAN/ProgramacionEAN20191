2e414fb20afe4700f831786b3a36450f20a480bd
Tu Nombre <you@example.com>
2019-03-05 19:52:38 -0500

    Agregados los ejercicios secundarios de estructuras de control con if

--- Semana_5/ejemplos_if_2.py
+++ Semana_5/ejemplos_if_2.py
@@ -37,4 +37,65 @@ def compara_edades(edad1, edad2):
     :param edad2: int la edad del segundo
     :return: mensaje asociado a la diferencia de edad
     """
-    pass
+    if edad1 > edad2:
+        return 'El segundo es mas joven'
+    elif edad1 == edad2:
+        return 'Tienen la misma edad'
+    else:
+        return 'El primero es mas joven'
+
+
+def es_parentesis(caracter):
+    """
+
+    (str of len == 1) -> str
+
+    >>> es_parentesis('(')
+    'Es parentesis'
+    >>> es_parentesis('x')
+    'No es parentesis'
+    >>> es_parentesis('xa')
+    Traceback (most recent call last):
+    ..
+    TypeError: xa no es un parentesis
+
+    :param caracter: str el caracter a evaluar
+    :return: str el mensaje de la validacion
+    """
+    if len(caracter) != 1:
+        raise TypeError(str(caracter) + ' no es un parentesis')
+    elif caracter in '()': # caracter == '(' or caracter == ')':
+        return 'Es parentesis'
+    return 'No es parentesis'
+
+
+def dividir(dividendo, divisor):
+    '''
+
+    (num, num) -> num
+
+    Divide un numero entre otro
+
+    >>> dividir(6, 2)
+    3.0
+    >>> dividir(1,0)
+    Traceback (most recent call last):
+    ..
+    ZeroDivisionError: No dividiras por 0
+    >>> dividir('hola', 100)
+    Traceback (most recent call last):
+    ..
+    TypeError: hola no es un numero
+
+    :param dividendo: num el dividendo a evaluar
+    :param divisor: num el divisor a evaluar
+    :return: la división entre dividendo y divisor
+    '''
+    if  int != type(dividendo) != float:
+        raise TypeError(str(dividendo) + ' no es un numero')
+    elif int != type(divisor) != float:
+        raise TypeError(str(divisor) + ' no es un numero')
+    elif divisor == 0:
+        print('al pelo')
+        raise ZeroDivisionError('No dividiras por 0')
+    return dividendo / divisor
